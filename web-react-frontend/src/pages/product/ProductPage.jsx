c7f1c02dadb981e2c97a5c94b502a58b475ef80d
ArtemK123 <darkmode1012@gmail.com>
2021-08-13 20:04:10 +0300

    Added navigation to product page

--- web-react-frontend/src/pages/product/ProductPage.jsx
+++ web-react-frontend/src/pages/product/ProductPage.jsx
@@ -1,28 +1,33 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import BackendService from "../../services/backendService";
+import ProductNavigation from "./ProductNavigation";
+
+const fetchProductInfoAsync = async (setProductInfo) => {
+  const productIdSearchParamKey = "id";
+  const searchParams = new URLSearchParams(window.location.search);
+  const backendService = new BackendService();
+  const productId = searchParams.get(productIdSearchParamKey);
+  const fetchedProductInfo = await backendService.getProductAsync(productId)
+  setProductInfo(fetchedProductInfo);
+}
+
+const createProductComponent = (productInfo) => (
+  <div>
+    <h3>{productInfo.ProductName}</h3>
+    <p>Id: {productInfo.Id}</p>
+    <p>Ingredients:<br/>{productInfo.Ingredients}</p>
+  </div>);
 
 export default function ProductPage() {
   const [productInfo, setProductInfo] = useState(undefined);
 
   if (productInfo === undefined) {
-    const productIdSearchParamKey = "id";
-    const searchParams = new URLSearchParams(window.location.search);
-    const backendService = new BackendService();
-    const productId = searchParams.get(productIdSearchParamKey);
-    backendService.getProductAsync(productId)
-      .then(fetchedProductInfo => setProductInfo(fetchedProductInfo));
-  }
-
-  if (productInfo === undefined) {
-    return (<p>Loading...</p>);
+    fetchProductInfoAsync(setProductInfo);
   }
 
   return (
     <div>
-      <h3>{productInfo.ProductName}</h3>
-      <p>Id: {productInfo.Id}</p>
-      <p>Ingredients:<br/>{productInfo.Ingredients}</p>
-    </div>
-  );
+      <ProductNavigation/>
+      {productInfo === undefined ? (<p>Loading...</p>) : createProductComponent(productInfo)}
+    </div>);
 }
